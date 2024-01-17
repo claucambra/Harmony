68bf8ecf5a47ce64865f8f77ba49fbaa36ebede9
Claudio Cambra <developer@claudiocambra.com>
2024-01-17 17:25:48 +0800

    Add simple tests for playable file extensions
    Signed-off-by: Claudio Cambra <developer@claudiocambra.com>

--- HarmonyTests/Common/AudioFileTest.swift
+++ HarmonyTests/Common/AudioFileTest.swift
@@ -5,4 +5,23 @@
 //  Created by Claudio Cambra on 17/1/24.
 //
 
-import Foundation
+import XCTest
+@testable import Harmony
+
+class AudioFileTests: XCTestCase {
+    func testPlayableFileExtensionsNotEmpty() {
+        let extensions = playableFileExtensions()
+        XCTAssertFalse(extensions.isEmpty,
+                       "The list of playable file extensions should not be empty.")
+    }
+
+    func testPlayableFileExtensionsContainCommonFormats() {
+        let extensions = playableFileExtensions()
+        let commonExtensions = ["mp3", "wav", "m4a", "aac", "flac"]
+        for ext in commonExtensions {
+            XCTAssertTrue(extensions.contains(ext),
+                          "\(ext) should be a recognized playable file extension.")
+        }
+    }
+
+}
