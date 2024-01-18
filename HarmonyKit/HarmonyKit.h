4eaac7f5a27c57306264bce1d060cdb0a548818f
Claudio Cambra <developer@claudiocambra.com>
2024-01-18 20:54:04 +0800

    Make HarmonyKit a framework
    Signed-off-by: Claudio Cambra <developer@claudiocambra.com>

---
+++ HarmonyKit/HarmonyKit.h
@@ -0,0 +1,18 @@
+//
+//  HarmonyKit.h
+//  HarmonyKit
+//
+//  Created by Claudio Cambra on 18/1/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HarmonyKit.
+FOUNDATION_EXPORT double HarmonyKitVersionNumber;
+
+//! Project version string for HarmonyKit.
+FOUNDATION_EXPORT const unsigned char HarmonyKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HarmonyKit/PublicHeader.h>
+
+
