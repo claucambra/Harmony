6043905ede2c58d29e3e65471e466959a3646037
Claudio Cambra <developer@claudiocambra.com>
2024-01-19 20:22:20 +0800

    Add initialiser for song from AVAsset
    Signed-off-by: Claudio Cambra <developer@claudiocambra.com>

--- HarmonyKit/Common/Song.swift
+++ HarmonyKit/Common/Song.swift
@@ -19,4 +19,44 @@ public class Song {
     var contributor: String?
     var type: String?
     var duration: CMTime?
+
+    public init?(fromAsset asset: AVAsset, withIdentifier id: String) async {
+        guard let metadata = try? await asset.load(.commonMetadata) else {
+            Logger.defaultLog.log("Could not get metadata for asset \(asset)")
+            return nil
+        }
+
+        for item in metadata {
+            if let value = try? await item.load(.value) as? String {
+                switch item.commonKey?.rawValue {
+                case "title":
+                    title = value
+                    break
+                case "albumName":
+                    album = value
+                    break
+                case "artist":
+                    artist = value
+                    break
+                case "creator":
+                    creator = value
+                    break
+                case "subject":
+                    subject = value
+                    break
+                case "contributor":
+                    contributor = value
+                    break
+                case "type":
+                    type = value
+                    break
+                default:
+                    break
+                }
+            }
+        }
+
+        identifier = id
+        duration = try? await asset.load(.duration)
+    }
 }
