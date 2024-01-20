907bd3611181951f563d9d602425bbe9c3170c73
Claudio Cambra <developer@claudiocambra.com>
2024-01-20 17:09:22 +0800

    Add a BackendsSettingsView
    Signed-off-by: Claudio Cambra <developer@claudiocambra.com>

---
+++ Harmony/Settings/BackendsSettingsView.swift
@@ -0,0 +1,48 @@
+//
+//  BackendsSettingsView.swift
+//  Harmony
+//
+//  Created by Claudio Cambra on 20/1/24.
+//
+
+import SwiftUI
+
+struct BackendConfig: Hashable {
+    let name: String
+    let detailString: String
+    let state: String
+}
+
+struct BackendsSettingsView: View {
+    private var configuredBackends: [BackendConfig] = []
+
+    var body: some View {
+        VStack {
+            listView.padding(10)
+            Button(action: {
+
+            }) {
+                Label("Configure new backend...", systemImage: "plus.circle")
+            }
+        }
+    }
+
+    @ViewBuilder
+    var listView: some View {
+        if configuredBackends.isEmpty {
+            emptyListView
+        } else {
+            objectsListView
+        }
+    }
+
+    var emptyListView: some View {
+        Text("No configured backends.")
+    }
+
+    var objectsListView: some View {
+        List(configuredBackends, id: \.self) { backend in
+            Text(backend.name)
+        }
+    }
+}
