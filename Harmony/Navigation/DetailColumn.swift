151b9691357935db94826a4cb007f1fcf98386dd
Claudio Cambra <developer@claudiocambra.com>
2024-01-20 16:11:29 +0800

    Add a DetailColumn component
    Signed-off-by: Claudio Cambra <developer@claudiocambra.com>

---
+++ Harmony/Navigation/DetailColumn.swift
@@ -0,0 +1,42 @@
+//
+//  DetailColumn.swift
+//  Harmony
+//
+//  Created by Claudio Cambra on 20/1/24.
+//
+
+import HarmonyKit
+import SwiftUI
+
+struct DetailColumn: View {
+    @Binding var selection: Panel?
+
+    var body: some View {
+        switch selection ?? .songs {
+        case .songs:
+            SongsTable(
+                model: SongsModel(
+                    withBackends: [
+                        LocalBackend(
+                            path: URL(fileURLWithPath: "/Users/claucambra/Downloads/TestMP3s")
+                        ) // TODO
+                    ]
+                ),
+                selection: .constant([])
+            )
+        }
+    }
+}
+
+struct DetailColumn_Previews: PreviewProvider {
+    struct Preview: View {
+        @State private var selection: Panel? = .songs
+
+        var body: some View {
+            DetailColumn(selection: $selection)
+        }
+    }
+    static var previews: some View {
+        Preview()
+    }
+}
