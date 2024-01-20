8a0b3acdb9facdb0c037acf2a627bd43089f26ba
Claudio Cambra <developer@claudiocambra.com>
2024-01-20 17:55:46 +0800

    Add starter backend description
    Signed-off-by: Claudio Cambra <developer@claudiocambra.com>

---
+++ HarmonyKit/Backend/BackendDescription.swift
@@ -0,0 +1,12 @@
+//
+//  BackendDescription.swift
+//  HarmonyKit
+//
+//  Created by Claudio Cambra on 20/1/24.
+//
+
+import Foundation
+
+public struct BackendDescription {
+    let name: String
+}
