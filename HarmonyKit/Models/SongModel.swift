ba47ae14c4d2813ba595af4a1e2d5319d4db2af6
Claudio Cambra <developer@claudiocambra.com>
2024-01-20 15:13:16 +0800

    Make SongsModel a basic class to gather songs from backends
    Signed-off-by: Claudio Cambra <developer@claudiocambra.com>

--- HarmonyKit/Models/SongModel.swift
+++
@@ -1,8 +0,0 @@
-//
-//  SongModel.swift
-//  HarmonyKit
-//
-//  Created by Claudio Cambra on 18/1/24.
-//
-
-import Foundation
---
+++ HarmonyKit/Models/SongsModel.swift
@@ -0,0 +1,32 @@
+//
+//  SongsModel.swift
+//  HarmonyKit
+//
+//  Created by Claudio Cambra on 18/1/24.
+//
+
+import AVFoundation
+import Combine
+import SwiftUI
+
+public class SongsModel: ObservableObject {
+    @Published public var songs: [Song] = []
+
+    private var backends: [Backend] = []
+
+    public init(withBackends inBackends: [Backend]) {
+        backends = inBackends
+        Task {
+            await refresh()
+        }
+    }
+
+    public func refresh() async {
+        var allScannedSongs: [Song] = []
+        for backend in backends {
+            let scanSongs = await backend.scan()
+            allScannedSongs.append(contentsOf: scanSongs)
+        }
+        songs = allScannedSongs
+    }
+}
