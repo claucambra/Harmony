f14c1a25953ad2e7b46a8d39c5770d052168ea01
Claudio Cambra <developer@claudiocambra.com>
2024-01-20 17:10:41 +0800

    Add SettingsView to app settings
    Signed-off-by: Claudio Cambra <developer@claudiocambra.com>

--- Harmony/HarmonyApp.swift
+++ Harmony/HarmonyApp.swift
@@ -13,5 +13,10 @@ struct HarmonyApp: App {
         WindowGroup {
             ContentView()
         }
+        #if os(macOS)
+        Settings {
+            SettingsView()
+        }
+        #endif
     }
 }
