50043ae36b781b29c72780b9295de0a9dc4bcf14
Claudio Cambra <developer@claudiocambra.com>
2024-01-20 18:38:24 +0800

    Display available backends in backend choice view
    Signed-off-by: Claudio Cambra <developer@claudiocambra.com>

--- Harmony/Settings/BackendChoiceView.swift
+++ Harmony/Settings/BackendChoiceView.swift
@@ -9,8 +9,13 @@ import HarmonyKit
 import SwiftUI
 
 struct BackendChoiceView: View {
+    let availableBackends = HarmonyKit.availableBackends
+
     var body: some View {
         NavigationStack {
+            List(availableBackends, id: \.self) { backendDescription in
+                Label(backendDescription.name, systemImage: backendDescription.systemImageName)
+            }
         }
     }
 }
