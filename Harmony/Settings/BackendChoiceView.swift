da0e0e931b9d02c181e01ed4158d5d31785e47c0
Claudio Cambra <developer@claudiocambra.com>
2024-01-20 17:40:49 +0800

    Add a backend choice view
    Signed-off-by: Claudio Cambra <developer@claudiocambra.com>

---
+++ Harmony/Settings/BackendChoiceView.swift
@@ -0,0 +1,16 @@
+//
+//  BackendChoiceView.swift
+//  Harmony
+//
+//  Created by Claudio Cambra on 20/1/24.
+//
+
+import HarmonyKit
+import SwiftUI
+
+struct BackendChoiceView: View {
+    var body: some View {
+        NavigationStack {
+        }
+    }
+}
