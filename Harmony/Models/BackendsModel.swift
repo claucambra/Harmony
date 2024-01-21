1916f3a8508a9648f7510ad9fce9acad433acac8
Claudio Cambra <developer@claudiocambra.com>
2024-01-21 18:49:37 +0800

    Add a starter BackendsModel
    Signed-off-by: Claudio Cambra <developer@claudiocambra.com>

---
+++ Harmony/Models/BackendsModel.swift
@@ -0,0 +1,30 @@
+//
+//  BackendsModel.swift
+//  Harmony
+//
+//  Created by Claudio Cambra on 21/1/24.
+//
+
+import Foundation
+import HarmonyKit
+
+class BackendsModel: ObservableObject {
+    static let shared = BackendsModel()
+    @Published var configurations = existingConfigs()
+    @Published var backends: [Backend] = []
+
+    private init() {
+        updateBackends()
+    }
+
+    private func updateBackends() {
+        for backend in availableBackends {
+            for config in configurations {
+                guard let configuredBackend = backendFromId(backend.id, withConfig: config) else {
+                    continue
+                }
+                backends.append(configuredBackend)
+            }
+        }
+    }
+}
