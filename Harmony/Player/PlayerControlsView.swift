eb54340851a3f226c3984a0cd1cb7f606f120169
Claudio Cambra <developer@claudiocambra.com>
2024-01-25 20:00:52 +0800

    Add start playercontrolsview
    Signed-off-by: Claudio Cambra <developer@claudiocambra.com>

---
+++ Harmony/Player/PlayerControlsView.swift
@@ -0,0 +1,40 @@
+//
+//  PlayerControlsView.swift
+//  Harmony
+//
+//  Created by Claudio Cambra on 25/1/24.
+//
+
+import SwiftUI
+
+struct PlayerControlsView: View {
+    var body: some View {
+        HStack {
+            Button {
+                // TODO
+            } label: {
+                Label("Shuffle", systemImage: "shuffle")
+            }
+            Button {
+                // TODO
+            } label: {
+                Label("Previous", systemImage: "backward.fill")
+            }
+            Button {
+                
+            } label: {
+                Label("Play", systemImage: "play.fill")
+            }
+            Button {
+                // TODO
+            } label: {
+                Label("Next", systemImage: "forward.fill")
+            }
+            Button {
+                // TODO
+            } label: {
+                Label("Repeat", systemImage: "repeat")
+            }
+        }
+    }
+}
