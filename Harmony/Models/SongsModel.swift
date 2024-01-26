73e10d95686d93d4dcc38a37616588c2486e1f6a
Claudio Cambra <developer@claudiocambra.com>
2024-01-27 01:55:30 +0800

    Move SongsModel to Harmony, out of HarmonyKit
    Signed-off-by: Claudio Cambra <developer@claudiocambra.com>

--- Harmony/Models/SongsModel.swift
+++ Harmony/Models/SongsModel.swift
@@ -1,12 +1,13 @@
 //
 //  SongsModel.swift
-//  HarmonyKit
+//  Harmony
 //
 //  Created by Claudio Cambra on 18/1/24.
 //
 
 import AVFoundation
 import Combine
+import HarmonyKit
 import SwiftUI
 
 @MainActor
