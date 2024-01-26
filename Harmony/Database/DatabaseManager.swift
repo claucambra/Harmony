1cad2dc083425cdf24d68c16f7175e40baef2644
Claudio Cambra <developer@claudiocambra.com>
2024-01-27 02:53:35 +0800

    Add convenience methods to write songs to database
    Signed-off-by: Claudio Cambra <developer@claudiocambra.com>

--- Harmony/Database/DatabaseManager.swift
+++ Harmony/Database/DatabaseManager.swift
@@ -26,4 +26,26 @@ class DatabaseManager {
         let dbPath = db.configuration.fileURL?.path ?? "unknown"
         Logger.database.info("Started database manager with db: \(dbPath)")
     }
+
+    func writeSong(_ song: Song) {
+        let dbSong = DatabaseSong(fromSong: song)
+        do {
+            try realm.write {
+                realm.add(dbSong, update: .modified)
+            }
+        } catch let error {
+            Logger.database.error("Error writing song to database: \(error)")
+        }
+    }
+
+    func writeSongs(_ songs: [Song]) {
+        let dbSongs = DatabaseSong.fromSongs(songs)
+        do {
+            try realm.write {
+                realm.add(dbSongs, update: .modified)
+            }
+        } catch let error {
+            Logger.database.error("Error writing songs to database: \(error)")
+        }
+    }
 }
