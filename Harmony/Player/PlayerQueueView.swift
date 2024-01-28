72943d34907271c837ffff221802d59897e92661
Claudio Cambra <developer@claudiocambra.com>
2024-01-29 01:36:02 +0800

    Bold current song in player queue view
    Signed-off-by: Claudio Cambra <developer@claudiocambra.com>

--- Harmony/Player/PlayerQueueView.swift
+++ Harmony/Player/PlayerQueueView.swift
@@ -8,12 +8,14 @@
 import SwiftUI
 
 struct PlayerQueueView: View {
+    @ObservedObject var playerController = PlayerController.shared
     @ObservedObject var queue = PlayerController.shared.queue
 
     var body: some View {
         List {
             ForEach(queue.songs) { song in
                 Text(song.title)
+                    .bold(playerController.currentSong?.identifier == song.identifier)
                     .onAppear {
                         queue.loadNextPageIfNeeded(song: song)
                     }
