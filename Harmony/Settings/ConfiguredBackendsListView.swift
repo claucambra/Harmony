b7cd62d329a5b9b6bff7844d03febed60764281b
Claudio Cambra <developer@claudiocambra.com>
2024-01-28 16:26:12 +0800

    Add a better-suited view for macOS on the configured backends list view
    Signed-off-by: Claudio Cambra <developer@claudiocambra.com>

--- Harmony/Settings/ConfiguredBackendsListView.swift
+++ Harmony/Settings/ConfiguredBackendsListView.swift
@@ -12,6 +12,53 @@ struct ConfiguredBackendsListView: View {
     @ObservedObject var backendsModel = BackendsModel.shared
     @State private var selection = Set<BackendPresentable.ID>()
 
+    #if os(macOS)
+    var body: some View {
+        List {
+            ForEach($backendsModel.backendPresentables) { backendPresentable in
+                let presentable = backendPresentable.wrappedValue
+                Button {
+                    guard let backend = backendsModel.backends[presentable.id] else {
+                        return
+                    }
+                    let window = NSPanel(
+                        contentRect: NSRect(x: 0, y: 0, width: 320, height: 240),
+                        styleMask: [.fullSizeContentView, .closable, .resizable, .titled],
+                        backing: .buffered, defer: false
+                    )
+                    window.contentView = NSHostingView(rootView: BackendConfigurationView(
+                        backendDescription: backend.typeDescription,
+                        configValues: backend.configValues
+                    ))
+                    window.makeKeyAndOrderFront(nil)
+                } label: {
+                    ConfiguredBackendsListItemView(backendPresentation: presentable)
+                        .swipeActions(edge: .leading) {
+                            Button {
+                                Task {
+                                    guard let backend = backendsModel.backends[presentable.id] else {
+                                        return
+                                    }
+                                    await SyncController.shared.syncBackend(backend)
+                                }
+                            } label: {
+                                Label("Sync", systemImage: "arrow.triangle.2.circlepath")
+                            }
+                        }
+                }
+                .buttonStyle(.plain)
+            }
+            .onDelete(perform: { indexSet in
+                for index in indexSet {
+                    let backend = backendsModel.backends.values[index]
+                    deleteBackendConfig(
+                        id: backend.id, withBackendDescriptionId: backend.typeDescription.id
+                    )
+                }
+            })
+        }
+    }
+    #else
     var body: some View {
         List(selection: $selection) {
             ForEach($backendsModel.backendPresentables) { backendPresentable in
@@ -49,4 +96,5 @@ struct ConfiguredBackendsListView: View {
             })
         }
     }
+    #endif
 }
