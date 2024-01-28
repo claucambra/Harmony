fae7a6cfc723315e72446130c9fb3263470d407a
Claudio Cambra <developer@claudiocambra.com>
2024-01-28 16:45:19 +0800

    Eliminate duplication in ConfiguredBackendsListView
    Signed-off-by: Claudio Cambra <developer@claudiocambra.com>

--- Harmony/Settings/ConfiguredBackendsListView.swift
+++ Harmony/Settings/ConfiguredBackendsListView.swift
@@ -12,41 +12,23 @@ struct ConfiguredBackendsListView: View {
     @ObservedObject var backendsModel = BackendsModel.shared
     @State private var selection = Set<BackendPresentable.ID>()
 
-    #if os(macOS)
     var body: some View {
         List {
             ForEach($backendsModel.backendPresentables) { backendPresentable in
                 let presentable = backendPresentable.wrappedValue
-                Button {
-                    guard let backend = backendsModel.backends[presentable.id] else {
-                        return
-                    }
-                    let window = NSPanel(
-                        contentRect: NSRect(x: 0, y: 0, width: 320, height: 240),
-                        styleMask: [.fullSizeContentView, .closable, .resizable, .titled],
-                        backing: .buffered, defer: false
-                    )
-                    window.contentView = NSHostingView(rootView: BackendConfigurationView(
-                        backendDescription: backend.typeDescription,
-                        configValues: backend.configValues
-                    ))
-                    window.makeKeyAndOrderFront(nil)
-                } label: {
-                    ConfiguredBackendsListItemView(backendPresentation: presentable)
-                        .swipeActions(edge: .leading) {
-                            Button {
-                                Task {
-                                    guard let backend = backendsModel.backends[presentable.id] else {
-                                        return
-                                    }
-                                    await SyncController.shared.syncBackend(backend)
+                listItemView(presentable: presentable)
+                    .swipeActions(edge: .leading) {
+                        Button {
+                            Task {
+                                guard let backend = backendsModel.backends[presentable.id] else {
+                                    return
                                 }
-                            } label: {
-                                Label("Sync", systemImage: "arrow.triangle.2.circlepath")
+                                await SyncController.shared.syncBackend(backend)
                             }
+                        } label: {
+                            Label("Sync", systemImage: "arrow.triangle.2.circlepath")
                         }
-                }
-                .buttonStyle(.plain)
+                    }
             }
             .onDelete(perform: { indexSet in
                 for index in indexSet {
@@ -58,43 +40,41 @@ struct ConfiguredBackendsListView: View {
             })
         }
     }
-    #else
-    var body: some View {
-        List(selection: $selection) {
-            ForEach($backendsModel.backendPresentables) { backendPresentable in
-                let presentable = backendPresentable.wrappedValue
-                NavigationLink {
-                    if let backend = backendsModel.backends[presentable.id] {
-                        BackendConfigurationView(
-                            backendDescription: backend.typeDescription,
-                            configValues: backend.configValues
-                        )
-                    }
-                } label: {
-                    ConfiguredBackendsListItemView(backendPresentation: presentable)
-                }
-                .swipeActions(edge: .leading) {
-                    Button {
-                        Task {
-                            guard let backend = backendsModel.backends[presentable.id] else {
-                                return
-                            }
-                            await SyncController.shared.syncBackend(backend)
-                        }
-                    } label: {
-                        Label("Sync", systemImage: "arrow.triangle.2.circlepath")
-                    }
-                }
-            }
-            .onDelete(perform: { indexSet in
-                for index in indexSet {
-                    let backend = backendsModel.backends.values[index]
-                    deleteBackendConfig(
-                        id: backend.id, withBackendDescriptionId: backend.typeDescription.id
-                    )
-                }
-            })
-        }
+
+    func openConfigWindowForBackend(_ backend: any Backend) {
+        let window = NSPanel(
+            contentRect: NSRect(x: 0, y: 0, width: 320, height: 240),
+            styleMask: [.fullSizeContentView, .closable, .resizable, .titled],
+            backing: .buffered, defer: false
+        )
+        window.contentView = NSHostingView(rootView: BackendConfigurationView(
+            backendDescription: backend.typeDescription,
+            configValues: backend.configValues
+        ).padding(20))
+        window.makeKeyAndOrderFront(nil)
     }
-    #endif
+
+    @ViewBuilder
+    private func listItemView(presentable: BackendPresentable) -> some View {
+        #if os(macOS)
+        Button {
+            guard let backend = backendsModel.backends[presentable.id] else { return }
+            openConfigWindowForBackend(backend)
+        } label: {
+            ConfiguredBackendsListItemView(backendPresentation: presentable)
+        }
+        .buttonStyle(.plain)
+        #else
+        NavigationLink {
+            if let backend = backendsModel.backends[presentable.id] {
+                BackendConfigurationView(
+                    backendDescription: backend.typeDescription,
+                    configValues: backend.configValues
+                )
+            }
+        } label: {
+            ConfiguredBackendsListItemView(backendPresentation: presentable)
+        }
+        #endif
+    }
 }
