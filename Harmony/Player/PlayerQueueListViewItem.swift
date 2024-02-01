1a2f84f183fc40862fc55eb6694b430804c85060
Claudio Cambra <developer@claudiocambra.com>
2024-02-01 17:12:26 +0800

    Add subtle border around artwork in player queue list view item
    Signed-off-by: Claudio Cambra <developer@claudiocambra.com>

--- Harmony/Player/PlayerQueueListViewItem.swift
+++ Harmony/Player/PlayerQueueListViewItem.swift
@@ -11,12 +11,19 @@ import SwiftUI
 struct PlayerQueueListViewItem: View {
     @ObservedObject var playerController = PlayerController.shared
     let song: Song
+    // TODO: Standardise measurements below
+    let borderRadius = 5.0
+    let borderWidth = 1.0
 
     var body: some View {
         HStack {
             SongArtworkView(song: song)
                 .frame(height: 40)
-                .clipShape(.rect(cornerRadius: 5.0))
+                .clipShape(.rect(cornerRadius: borderRadius))
+                .overlay(
+                    RoundedRectangle(cornerRadius: borderRadius)
+                        .stroke(.separator, lineWidth: borderWidth)
+                )
             VStack(alignment: .leading) {
                 Text(song.title)
                     .lineLimit(1)
