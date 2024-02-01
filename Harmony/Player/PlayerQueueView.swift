b01416012c309ef982a7efe7777590998c718b3b
Claudio Cambra <developer@claudiocambra.com>
2024-02-01 17:49:48 +0800

    Enable interaction with queue items in player queue
    Signed-off-by: Claudio Cambra <developer@claudiocambra.com>

--- Harmony/Player/PlayerQueueView.swift
+++ Harmony/Player/PlayerQueueView.swift
@@ -5,19 +5,27 @@
 //  Created by Claudio Cambra on 26/1/24.
 //
 
+import HarmonyKit
 import SwiftUI
 
 struct PlayerQueueView: View {
     @ObservedObject var playerController = PlayerController.shared
     @ObservedObject var queue = PlayerController.shared.queue
+    @State private var selection: Set<Song.ID> = []
 
     var body: some View {
-        List {
+        List(selection: $selection) {
             ForEach(queue.songs) { song in
                 PlayerQueueListViewItem(song: song)
                     .onAppear {
                         queue.loadNextPageIfNeeded(song: song)
                     }
+            }
+        }
+        .contextMenu(forSelectionType: Song.ID.self) { ids in
+        } primaryAction: { ids in
+            for id in ids {
+                PlayerController.shared.playSongFromQueue(instanceId: id)
             }
         }
     }
