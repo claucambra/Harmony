9b54855fd7c8e3022dcb69954f6f4447fa3cbe93
Claudio Cambra <developer@claudiocambra.com>
2024-02-17 15:41:17 +0800

    Add starter resource loader delegate for Nextcloud backend
    Signed-off-by: Claudio Cambra <developer@claudiocambra.com>

---
+++ HarmonyKit/Backend/Nextcloud/NextcloudAVAssetResourceLoaderDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  NextcloudAVAssetResourceLoaderDelegate.swift
+//  HarmonyKit
+//
+//  Created by Claudio Cambra on 17/2/24.
+//
+
+import AVFoundation
+
+class NextcloudAVAssetResourceLoaderDelegate: NSObject, AVAssetResourceLoaderDelegate {
+
+}
