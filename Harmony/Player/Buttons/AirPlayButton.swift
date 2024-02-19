9af1f591278a4a73b3d7a236fa600a72bb66419a
Claudio Cambra <developer@claudiocambra.com>
2024-02-19 18:47:12 +0800

    Add an AirPlay button
    Signed-off-by: Claudio Cambra <developer@claudiocambra.com>

---
+++ Harmony/Player/Buttons/AirPlayButton.swift
@@ -0,0 +1,48 @@
+//
+//  AirPlayButton.swift
+//  Harmony
+//
+//  Created by Claudio Cambra on 19/2/24.
+//
+
+import SwiftUI
+
+import AVKit
+
+#if os(macOS)
+fileprivate struct InternalMacAirPlayButton: NSViewRepresentable {
+    func makeNSView(context: Context) -> some NSView {
+        let routePickerView = AVRoutePickerView()
+        routePickerView.isRoutePickerButtonBordered = false
+        routePickerView.player = PlayerController.shared.avPlayer
+        return AVRoutePickerView()
+    }
+
+    func updateNSView(_ nsView: NSViewType, context: Context) {
+        return
+    }
+}
+#endif
+
+struct AirPlayButton: View {
+    #if !os(macOS)
+    private let internalButton = AVRoutePickerView()
+    #endif
+
+    var body: some View {
+        #if os(macOS)
+        InternalMacAirPlayButton()
+        #else
+        Button {
+            for view: UIView in internalButton.subviews {
+                if let button = view as? UIButton {
+                    button.sendActions(for: .touchUpInside)
+                    break
+                }
+            }
+        } label: {
+            Label("Choose output device", systemImage: "airplayaudio")
+        }
+        #endif
+    }
+}
