9217d40b14b10444de7ac401f04e973083e05a48
Claudio Cambra <developer@claudiocambra.com>
2024-02-19 16:46:02 +0800

    Add option to selectively clip corners of LoadingIndicatorOverlayView
    Signed-off-by: Claudio Cambra <developer@claudiocambra.com>

--- Harmony/Common/LoadingIndicatorOverlayView.swift
+++ Harmony/Common/LoadingIndicatorOverlayView.swift
@@ -8,11 +8,22 @@
 import SwiftUI
 
 struct LoadingIndicatorOverlayView: View {
+    var topLeadingRadius: CGFloat = UIMeasurements.cornerRadius
+    var bottomLeadingRadius: CGFloat = UIMeasurements.cornerRadius
+    var bottomTrailingRadius: CGFloat = UIMeasurements.cornerRadius
+    var topTrailingRadius: CGFloat = UIMeasurements.cornerRadius
+
     var body: some View {
         ZStack(alignment: .center) {
-            RoundedRectangle(cornerRadius: UIMeasurements.cornerRadius)
+            Rectangle()
                 .foregroundStyle(.regularMaterial)
                 .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .clipShape(.rect(cornerRadii: .init(
+                    topLeading: topLeadingRadius,
+                    bottomLeading: bottomLeadingRadius,
+                    bottomTrailing: bottomTrailingRadius,
+                    topTrailing: topTrailingRadius
+                )))
             ProgressView()
                 .padding(.all, UIMeasurements.mediumPadding)
                 .frame(maxWidth: .infinity, maxHeight: .infinity)
