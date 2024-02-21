2f6ea018fd96811b7ab4abc3fb3c104b573ca2bd
Claudio Cambra <developer@claudiocambra.com>
2024-02-22 02:35:58 +0800

    Implement application metadata block
    Signed-off-by: Claudio Cambra <developer@claudiocambra.com>

--- HarmonyKit/Backend/FLAC/Metadata/Blocks/FLACApplicationMetadataBlock.swift
+++ HarmonyKit/Backend/FLAC/Metadata/Blocks/FLACApplicationMetadataBlock.swift
@@ -8,5 +8,14 @@
 import Foundation
 
 struct FLACApplicationMetadataBlock {
+    public let header: FLACMetadataBlockHeader
+    public let appId: String
+    public let data: Data
 
+    init(bytes: Data, header: FLACMetadataBlockHeader) {
+        self.header = header
+        let value = bytes[0..<4]
+        appId = String(bytes: value, encoding: .ascii) ?? "\(value)"
+        data = Data(bytes[4..<Int(header.metadataBlockDataSize)])
+    }
 }
