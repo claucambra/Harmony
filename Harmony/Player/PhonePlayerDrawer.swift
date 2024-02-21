de44677a26bbe66b488d6ddb38ce275d1bcdbb56
Claudio Cambra <developer@claudiocambra.com>
2024-02-21 14:23:03 +0800

    Replace average colour with blurred covert art for phoneplayerdrawer
    Signed-off-by: Claudio Cambra <developer@claudiocambra.com>

--- Harmony/Player/PhonePlayerDrawer.swift
+++ Harmony/Player/PhonePlayerDrawer.swift
@@ -12,46 +12,6 @@ struct PhonePlayerDrawer: View {
     @Environment(\.colorScheme) var colorScheme
     @ObservedObject private var controller = PlayerController.shared
     @State private var queueVisible = false
-    private var backgroundColor: Color? {
-        guard let currentSong = controller.currentSong,
-              let artworkData = currentSong.artwork,
-              let inputImage = CIImage(data: artworkData)
-        else {
-            return nil
-        }
-
-        let extentVector = CIVector(
-            x: inputImage.extent.origin.x,
-            y: inputImage.extent.origin.y,
-            z: inputImage.extent.size.width,
-            w: inputImage.extent.size.height
-        )
-
-        guard let filter = CIFilter(
-            name: "CIAreaAverage",
-            parameters: [kCIInputImageKey: inputImage, kCIInputExtentKey: extentVector]
-        ), let outputImage = filter.outputImage else {
-            return nil
-        }
-
-        var bitmap = [UInt8](repeating: 0, count: 4)
-        let context = CIContext(options: [.workingColorSpace: kCFNull as Any])
-        context.render(
-            outputImage,
-            toBitmap: &bitmap,
-            rowBytes: 4,
-            bounds: CGRect(x: 0, y: 0, width: 1, height: 1),
-            format: .RGBA8,
-            colorSpace: nil
-        )
-
-        let opacity = colorScheme == .dark ? 0.4 : 0.2
-        return Color(
-            red: CGFloat(bitmap[0]) / 255,
-            green: CGFloat(bitmap[1]) / 255,
-            blue: CGFloat(bitmap[2]) / 255
-        ).opacity(opacity)
-    }
 
     let cornerRadius = UIMeasurements.cornerRadius
     let borderWidth = UIMeasurements.thinBorderWidth
@@ -124,9 +84,17 @@ struct PhonePlayerDrawer: View {
         .presentationDetents([.large])
         .presentationDragIndicator(.visible)
         .background {
-            backgroundColor
-                .animation(Animation.linear(duration: 0.3))
+            withAnimation(.linear(duration: 0.3)) {
+                ZStack {
+                    if let currentSong = controller.currentSong {
+                        SongArtworkView(song: currentSong, scaleMode: .fill)
+                            .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    }
+                    Rectangle()
+                        .foregroundStyle(.thickMaterial)
+                }
                 .ignoresSafeArea()
+            }
         }
     }
 
