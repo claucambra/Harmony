13de7707c4006e3254a3be92fd4e51363b07c36d
Claudio Cambra <developer@claudiocambra.com>
2024-02-22 01:01:18 +0800

    Add starter FLAC metadata parser
    Signed-off-by: Claudio Cambra <developer@claudiocambra.com>

---
+++ HarmonyKit/Common/FLACParser.swift
@@ -0,0 +1,20 @@
+//
+//  FLACParser.swift
+//  HarmonyKit
+//
+//  Created by Claudio Cambra on 22/2/24.
+//
+
+import Foundation
+
+class FLACParser {
+    let data: Data
+
+    var isFLAC: Bool {
+        String(data: data[0 ..< 4], encoding: .ascii) == "fLaC"
+    }
+
+    init(data: Data) {
+        self.data = data
+    }
+}
