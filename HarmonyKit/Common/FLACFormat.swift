bfd7f12326477dc5a9a83ab2cf7226f5e40b39a8
Claudio Cambra <developer@claudiocambra.com>
2024-02-22 01:18:11 +0800

    Add some more flac format info
    Signed-off-by: Claudio Cambra <developer@claudiocambra.com>

--- HarmonyKit/Common/FLACFormat.swift
+++ HarmonyKit/Common/FLACFormat.swift
@@ -9,4 +9,19 @@ import Foundation
 
 struct FLACFormat {
     static var streamMarker = "fLaC"
+
+    struct Header {
+        static let size = 4
+
+        enum BlockType: UInt8 {
+            case streamInfo
+            case padding
+            case application
+            case seekTable
+            case vorbisComment
+            case cueSheet
+            case picture
+            case reserved
+        }
+    }
 }
