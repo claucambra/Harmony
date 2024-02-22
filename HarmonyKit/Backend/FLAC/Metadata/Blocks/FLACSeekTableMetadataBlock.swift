9b45787f196137a07cf1afbcce37f82d89bac751
Claudio Cambra <developer@claudiocambra.com>
2024-02-22 22:04:25 +0800

    Implement seek table metadata block
    Signed-off-by: Claudio Cambra <developer@claudiocambra.com>

--- HarmonyKit/Backend/FLAC/Metadata/Blocks/FLACSeekTableMetadataBlock.swift
+++ HarmonyKit/Backend/FLAC/Metadata/Blocks/FLACSeekTableMetadataBlock.swift
@@ -8,5 +8,34 @@
 import Foundation
 
 struct FLACSeekTableMetadataBlock {
-    
+    struct SeekPoint: Hashable {
+        static let size = 8 + 8 + 2
+        static let placeholder: UInt64 = 0xFFFF_FFFF_FFFF_FFFF
+        let sampleNumber: UInt64
+        let streamOffset: UInt64
+        let frameSamples: UInt32
+
+        init(bytes: Data) {
+            sampleNumber = bytes[0..<8].withUnsafeBytes { $0.load(as: UInt64.self) }
+            streamOffset = bytes[8..<16].withUnsafeBytes { $0.load(as: UInt64.self) }
+            frameSamples = bytes[16..<18].withUnsafeBytes { $0.load(as: UInt32.self) }
+        }
+    }
+
+    let header: FLACMetadataBlockHeader
+    let points: [SeekPoint]
+
+    init(bytes: Data, header: FLACMetadataBlockHeader) {
+        self.header = header
+
+        let pointCount = Int(header.metadataBlockDataSize) / SeekPoint.size
+        var pointTable: [SeekPoint] = []
+        for i in 0..<pointCount {
+            let pointStartByteIndex = i * SeekPoint.size
+            let pointEndByteIndex = pointStartByteIndex + SeekPoint.size - 1
+            let point = SeekPoint(bytes: bytes[pointStartByteIndex...pointEndByteIndex])
+            pointTable.append(point)
+        }
+        points = pointTable
+    }
 }
