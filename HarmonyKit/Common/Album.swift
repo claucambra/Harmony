2b73b0de5af3ea15ed6b86d1c43ce74ca0cb5244
Claudio Cambra <developer@claudiocambra.com>
2024-02-29 01:59:45 +0800

    Implement proper Album data model
    Signed-off-by: Claudio Cambra <developer@claudiocambra.com>

--- HarmonyKit/Common/Album.swift
+++ HarmonyKit/Common/Album.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+import SwiftData
+
+@Model
+public final class Album {
+    public var songs: [Song] = []
+    @Attribute(.unique) public var title: String
+    public var artist: String?
+    public var genre: String?
+    public var year: Int?
+    @Attribute(.externalStorage) public var artwork: Data?
+
+
+    public init?(songs: [Song]) {
+        guard let referenceSong = songs.first else { return nil }
+        title = referenceSong.album
+        artist = referenceSong.artist
+        genre = referenceSong.genre
+        // year = referenceSong.year
+    }
+}
