aff6a34447e12ac84d3931b576235cbb518b9c8c
Claudio Cambra <developer@claudiocambra.com>
2024-02-29 15:57:56 +0800

    Add convenience bordered artwork view
    Signed-off-by: Claudio Cambra <developer@claudiocambra.com>

--- Harmony/Common/BorderedArtworkView.swift
+++ Harmony/Common/BorderedArtworkView.swift
@@ -5,4 +5,21 @@
 //  Created by Claudio Cambra on 29/2/24.
 //
 
-import Foundation
+import SwiftUI
+
+struct BorderedArtworkView: View {
+    let artwork: Data?
+    let borderWidth = UIMeasurements.thinBorderWidth
+    let cornerRadius = UIMeasurements.cornerRadius
+    let scaleMode = ArtworkView.ScaleMode.fit
+
+    var body: some View {
+        ArtworkView(artwork: artwork, scaleMode: scaleMode)
+            .clipShape(.rect(cornerRadius: cornerRadius))
+            .overlay(
+                RoundedRectangle(cornerRadius: cornerRadius)
+                    .stroke(.separator, lineWidth: borderWidth)
+            )
+
+    }
+}
