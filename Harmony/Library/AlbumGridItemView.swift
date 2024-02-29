7f68bd8b2715dde606af12b6c919a45e609fbffa
Claudio Cambra <developer@claudiocambra.com>
2024-02-29 19:36:33 +0800

    Display fallback strings in album item view
    Signed-off-by: Claudio Cambra <developer@claudiocambra.com>

--- Harmony/Library/AlbumGridItemView.swift
+++ Harmony/Library/AlbumGridItemView.swift
@@ -10,15 +10,21 @@ import SwiftUI
 
 struct AlbumGridItemView: View {
     let album: Album
+    let fallbackTitle = "Unknown album"
+    let fallbackArtist = "Unknown artist"
 
     var body: some View {
+        let titleString = album.title == "" ? fallbackTitle : album.title
+        let artistString = album.artist == nil || album.artist == ""
+            ? fallbackArtist
+            : album.artist ?? fallbackArtist
         VStack {
             BorderedArtworkView(artwork: album.artwork)
                 .frame(maxWidth: .infinity)
-            Text(album.title)
+            Text(titleString)
                 .lineLimit(1)
                 .frame(maxWidth: .infinity)
-            Text(album.artist ?? "Unknown artist")
+            Text(artistString)
                 .lineLimit(1)
                 .foregroundStyle(.secondary)
                 .frame(maxWidth: .infinity)
