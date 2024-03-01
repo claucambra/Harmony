bca4ebc9ac0f851eb44ad508746a00a2f022530a
Claudio Cambra <developer@claudiocambra.com>
2024-03-01 18:39:18 +0800

    Add ability to toggle viewing of artist and artwork on songlistitemview
    Signed-off-by: Claudio Cambra <developer@claudiocambra.com>

--- Harmony/Library/SongListItemView.swift
+++ Harmony/Library/SongListItemView.swift
@@ -10,18 +10,24 @@ import SwiftUI
 
 struct SongListItemView: View {
     @ObservedObject var song: Song
-    let isCurrentSong: Bool
+    @State var isCurrentSong = false
+    @State var displayArtwork = true
+    @State var displayArtist = true
 
     var body: some View {
         HStack {
-            BorderedArtworkView(artwork: song.artwork)
-                .frame(height: UIMeasurements.smallArtworkHeight)
+            if displayArtwork {
+                BorderedArtworkView(artwork: song.artwork)
+                    .frame(height: UIMeasurements.smallArtworkHeight)
+            }
             VStack(alignment: .leading) {
                 Text(song.title)
                     .lineLimit(1)
-                Text(song.artist)
-                    .lineLimit(1)
-                    .foregroundStyle(.secondary)
+                if displayArtist {
+                    Text(song.artist)
+                        .lineLimit(1)
+                        .foregroundStyle(.secondary)
+                }
             }
             .fixedSize(horizontal: false, vertical: true)
             Spacer()
