5d797462a638f4ce71a295412d87e3cc5bfde793
Claudio Cambra <developer@claudiocambra.com>
2024-03-01 17:49:34 +0800

    Add header to AlbumsGridView
    Signed-off-by: Claudio Cambra <developer@claudiocambra.com>

--- Harmony/Library/AlbumDetailView.swift
+++ Harmony/Library/AlbumDetailView.swift
@@ -13,7 +13,25 @@ struct AlbumDetailView: View {
 
     var body: some View {
         List {
-            Rectangle().foregroundStyle(.blue).frame(width: 29, height: 30)
+            HStack {
+                BorderedArtworkView(artwork: album.artwork)
+                    .frame(height: UIMeasurements.largeArtworkHeight)
+
+                VStack(alignment: .leading) {
+                    Text(album.title.isEmpty ? "Unknown album" : album.title)
+                        .font(.title)
+                        .bold()
+                        .multilineTextAlignment(.leading)
+                    Text(album.artist ?? "Unknown artist")
+                        .font(.title)
+                        .multilineTextAlignment(.leading)
+                    Text(album.genre == nil || album.genre!.isEmpty
+                            ? "Unknown genre"
+                            : album.genre ?? "Unknown genre")
+                        .font(.subheadline)
+                        .multilineTextAlignment(.leading)
+                }
+            }
             ForEach(album.songs) { song in
                 Text(song.title)
             }
