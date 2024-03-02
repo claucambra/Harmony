cce8f94b0cbfc965cc6bf9ebb4151bfef1046844
Claudio Cambra <developer@claudiocambra.com>
2024-03-02 15:23:11 +0800

    Add environment value for floating bar height
    Signed-off-by: Claudio Cambra <developer@claudiocambra.com>

--- Harmony/Extensions/EnvironmentValues+Extension.swift
+++ Harmony/Extensions/EnvironmentValues+Extension.swift
@@ -7,11 +7,19 @@
 
 import SwiftUI
 
+private struct FloatingBarHeightEnvironmentKey: EnvironmentKey {
+    static let defaultValue: CGFloat = 0.0
+}
+
 private struct SearchTextEnvironmentKey: EnvironmentKey {
     static let defaultValue: String = ""
 }
 
 extension EnvironmentValues {
+    var floatingBarHeight: CGFloat {
+        get { self[FloatingBarHeightEnvironmentKey.self] }
+        set { self[FloatingBarHeightEnvironmentKey.self] = newValue }
+    }
     var searchText: String {
         get { self[SearchTextEnvironmentKey.self] }
         set { self[SearchTextEnvironmentKey.self] = newValue }
