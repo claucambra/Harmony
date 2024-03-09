3d30adf2b4a1cd864d5f0b3337503e2f84b3c146
Claudio Cambra <developer@claudiocambra.com>
2024-03-10 02:21:33 +0800

    Fix crash on setting download state in ncbackend
    Signed-off-by: Claudio Cambra <developer@claudiocambra.com>

--- HarmonyKit/Backend/Nextcloud/NextcloudBackend.swift
+++ HarmonyKit/Backend/Nextcloud/NextcloudBackend.swift
@@ -236,9 +236,7 @@ public class NextcloudBackend: NSObject, Backend {
 
         do {
             try FileManager.default.removeItem(at: localUrl)
-            Task { @MainActor in
-                song.downloadState = DownloadState.notDownloaded.rawValue
-            }
+            song.downloadState = DownloadState.notDownloaded.rawValue
         } catch let error {
             Logger.ncBackend.error("Could not delete song \(song.url) at \(localUrl): \(error)")
         }
