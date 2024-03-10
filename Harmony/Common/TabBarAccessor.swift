24f99097d20586678fd0d04f1cfbaeab4d0615b9
Claudio Cambra <developer@claudiocambra.com>
2024-03-11 00:00:42 +0800

    Add TabBarAccessor component
    Signed-off-by: Claudio Cambra <developer@claudiocambra.com>

---
+++ Harmony/Common/TabBarAccessor.swift
@@ -0,0 +1,38 @@
+//
+//  TabBarAccessor.swift
+//  Harmony
+//
+//  Created by Claudio Cambra on 10/3/24.
+//
+
+import SwiftUI
+
+struct TabBarAccessor: UIViewControllerRepresentable {
+    var callback: (UITabBar) -> Void
+    private let proxyController = ViewController()
+
+    func makeUIViewController(
+        context: UIViewControllerRepresentableContext<TabBarAccessor>
+    ) -> UIViewController {
+        proxyController.callback = callback
+        return proxyController
+    }
+
+    func updateUIViewController(
+        _ uiViewController: UIViewController,
+        context: UIViewControllerRepresentableContext<TabBarAccessor>
+    ) {}
+
+    typealias UIViewControllerType = UIViewController
+
+    private class ViewController: UIViewController {
+        var callback: (UITabBar) -> Void = { _ in }
+
+        override func viewWillAppear(_ animated: Bool) {
+            super.viewWillAppear(animated)
+            if let tabBar = self.tabBarController {
+                self.callback(tabBar.tabBar)
+            }
+        }
+    }
+}
