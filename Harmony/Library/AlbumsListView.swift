d6fed78e02a9a983ba4fe108dbf7c90d04707f1b
Claudio Cambra <developer@claudiocambra.com>
2024-03-14 01:08:08 +0800

    Add a starter AlbumListView
    Signed-off-by: Claudio Cambra <developer@claudiocambra.com>

---
+++ Harmony/Library/AlbumsListView.swift
@@ -0,0 +1,44 @@
+//
+//  AlbumsListView.swift
+//  Harmony
+//
+//  Created by Claudio Cambra on 14/3/24.
+//
+
+import HarmonyKit
+import SwiftUI
+
+struct AlbumsListView: View {
+    @Environment(\.floatingBarHeight) var floatingBarHeight
+    let albums: [Album]
+    @Binding var searchText: String
+    @Binding var showOnlineSongs: Bool
+    @Binding var sortOrder: SortDescriptor<Album>
+
+    @State private var selection: Set<Album.ID> = []
+
+    var body: some View {
+        List(selection: $selection) {
+            ForEach(albums) { album in
+                Text(album.title)
+            }
+        }
+        #if !os(macOS)
+        .searchable(text: $searchText, placement: .navigationBarDrawer)
+        #endif
+        .toolbar {
+            ToolbarItem {
+                Menu {
+                    Toggle(isOn: $showOnlineSongs) {
+                        Label("Undownloaded songs", systemImage: "cloud")
+                    }
+                    Button("Title") { sortOrder = SortDescriptor(\Album.title) }
+                    Button("Artist") { sortOrder = SortDescriptor(\Album.artist) }
+                    Button("Year") { sortOrder = SortDescriptor(\Album.year) }
+                } label: {
+                    Label("Sort and filter", systemImage: "line.3.horizontal.decrease.circle")
+                }
+            }
+        }
+    }
+}
