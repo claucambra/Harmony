2a40020dde3b515b8eaae70bdfd9985013a87d7d
Claudio Cambra <developer@claudiocambra.com>
2024-03-14 18:13:54 +0800

    Fix artist view navigation on iPad
    Signed-off-by: Claudio Cambra <developer@claudiocambra.com>

--- Harmony/SplitContentView.swift
+++ Harmony/SplitContentView.swift
@@ -16,34 +16,26 @@ struct SplitContentView: View {
     @Binding var albumSort: SortDescriptor<Album>
     @Environment(\.floatingBarHeight) var floatingBarHeight: CGFloat
     @ObservedObject var syncController = SyncController.shared
+    @State var artistSelection: Artist?
+    @State var columnVisibility = NavigationSplitViewVisibility.all
     @State var settingsSheetVisible: Bool = false
 
     var body: some View {
+        #if os(macOS)
+        twoColumnView
+        #else
+        if selection == .artists {
+            threeColumnView
+        } else {
+            twoColumnView
+        }
+        #endif
+    }
+
+    @ViewBuilder
+    private var twoColumnView: some View {
         NavigationSplitView {
-            Sidebar(selection: $selection, showOnlineSongs: $showOnlineSongs)
-                .toolbar {
-                    ToolbarItemGroup {
-                        Button {
-                            Task {
-                                await syncController.sync()
-                            }
-                        } label: {
-                            Image(systemName: "arrow.triangle.2.circlepath.circle")
-                        }
-                        .disabled(syncController.currentlySyncingFully)
-                        #if !os(macOS)
-                        Button(action: {
-                            settingsSheetVisible.toggle()
-                        }) {
-                            Label("Settings", systemImage: "gear")
-                        }
-                        .sheet(isPresented: $settingsSheetVisible) {
-                            SettingsSheet()
-                        }
-                        #endif
-                    }
-                }
-                .safeAreaPadding([.bottom], floatingBarHeight)
+            navSideBar
         } detail: {
             NavigationStack(path: $path) {
                 DetailColumn(
@@ -55,5 +47,54 @@ struct SplitContentView: View {
             }
             .safeAreaPadding([.bottom], floatingBarHeight)
         }
+    }
+
+    @ViewBuilder
+    private var threeColumnView: some View {
+        NavigationSplitView(columnVisibility: $columnVisibility) {
+            navSideBar
+        } content: {
+            ArtistsListView(selection: $artistSelection)
+        } detail: {
+            NavigationStack(path: $path) {
+                if let artist = artistSelection {
+                    AlbumsListView(
+                        albums: artist.albums,
+                        searchText: $searchText,
+                        showOnlineSongs: $showOnlineSongs,
+                        sortOrder: $albumSort
+                    )
+                }
+            }
+            .safeAreaPadding([.bottom], floatingBarHeight)
+        }
+    }
+
+    @ViewBuilder
+    private var navSideBar: some View {
+        Sidebar(selection: $selection, showOnlineSongs: $showOnlineSongs)
+            .toolbar {
+                ToolbarItemGroup {
+                    Button {
+                        Task {
+                            await syncController.sync()
+                        }
+                    } label: {
+                        Image(systemName: "arrow.triangle.2.circlepath.circle")
+                    }
+                    .disabled(syncController.currentlySyncingFully)
+                    #if !os(macOS)
+                    Button(action: {
+                        settingsSheetVisible.toggle()
+                    }) {
+                        Label("Settings", systemImage: "gear")
+                    }
+                    .sheet(isPresented: $settingsSheetVisible) {
+                        SettingsSheet()
+                    }
+                    #endif
+                }
+            }
+            .safeAreaPadding([.bottom], floatingBarHeight)
     }
 }
