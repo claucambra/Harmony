3a474e3b1a187652fd6d7f7744430a666da526f2
Claudio Cambra <developer@claudiocambra.com>
2024-03-15 18:53:17 +0800

    Add explainer for container class
    Signed-off-by: Claudio Cambra <developer@claudiocambra.com>

--- HarmonyKit/Common/Container.swift
+++ HarmonyKit/Common/Container.swift
@@ -7,6 +7,12 @@
 
 import SwiftData
 
+/// Containers, which hold metadata for structures like folders, exist purely for scanning purposes.
+/// When dealing with file hierarchies, knowing the state of a given container can help speed up
+/// scan times significantly, as knowing the versionId of a container can sometimes help in knowing
+/// whether a given container needs to be scanned or not. In cases where knowing about the container
+/// state is not possible or not useful, backends can safely ignore them.
+
 @Model
 final public class Container {
     @Attribute(.unique) public var identifier = ""
