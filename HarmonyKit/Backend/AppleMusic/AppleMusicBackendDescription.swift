db529da937886771c38735734fa50d1eaa2f2f79
Claudio Cambra <developer@claudiocambra.com>
2025-05-10 23:10:01 +0800

    Add AppleMusicBackendDescription
    Signed-off-by: Claudio Cambra <developer@claudiocambra.com>

---
+++ HarmonyKit/Backend/AppleMusic/AppleMusicBackendDescription.swift
@@ -0,0 +1,14 @@
+//
+//  AppleMusicBackendDescription.swift
+//  Harmony
+//
+//  Created by Claudio Cambra on 10/5/25.
+//
+
+let appleMusicBackendTypeDescription = BackendDescription(
+    id: "applemusic-backend",
+    name: "Apple Music",
+    description: "Provides music stored in your Apple Music library.",
+    systemImageName: "music.note",
+    configDescription: []
+)
