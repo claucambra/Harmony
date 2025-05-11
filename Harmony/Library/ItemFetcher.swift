700e88d0ac16555bf7a09cbac014430e6a8fb8a7
Claudio Cambra <developer@claudiocambra.com>
2025-05-11 16:18:53 +0800

    Add ItemFetcher class
    Signed-off-by: Claudio Cambra <developer@claudiocambra.com>

---
+++ Harmony/Library/ItemFetcher.swift
@@ -0,0 +1,37 @@
+//
+//  SongsFetcher.swift
+//  Harmony
+//
+//  Created by Claudio Cambra on 11/5/25.
+//
+
+import HarmonyKit
+import SwiftData
+import SwiftUI
+
+class ItemFetcher<T> where T: PersistentModel {
+    @ModelActor
+    actor BackgroundActor {
+        func fetchItems(
+            predicate: Predicate<T>?, sortDescriptors: [SortDescriptor<T>]
+        ) throws -> [PersistentIdentifier] {
+            let backgroundContext = ModelContext(modelContainer)
+            let descriptor = FetchDescriptor<T>(predicate: predicate, sortBy: sortDescriptors)
+            let items = try backgroundContext.fetch(descriptor)
+            return items.map { $0.persistentModelID }
+        }
+    }
+
+    private let modelContainer: ModelContainer
+    private lazy var backgroundActor = BackgroundActor(modelContainer: modelContainer)
+
+    init(modelContainer: ModelContainer) {
+        self.modelContainer = modelContainer
+    }
+
+    func getItems(
+        predicate: Predicate<T>? = nil, sortDescriptors: [SortDescriptor<T>] = []
+    ) async throws -> [PersistentIdentifier] {
+        try await backgroundActor.fetchItems(predicate: predicate, sortDescriptors: sortDescriptors)
+    }
+}
